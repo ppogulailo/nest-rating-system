e545b53bccfabf16f8da90c38aa11d27373895dc
Pasha Pogulailo <you@example.com>
2022-12-05 19:41:44 +0200

    add schema model and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PageModule } from './page/page.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PageModule, ProductModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/page/page.model/page.model.ts
@@ -0,0 +1,27 @@
+export enum TopLevelCategory{
+  Courses,
+  Services,
+  Books,
+  Products,
+}
+
+export class PageModel {
+  firstCategory:TopLevelCategory;
+  secondCategory:string;
+  title:string;
+  category:string;
+  hh?:{
+    count:number;
+    juniorSalary:number;
+    middleSalary:number;
+    seniorSalary:number;
+  };
+  advantages:{
+    title:string;
+    description:string;
+
+  }[];
+  seoText:string;
+  tagsTitle:string;
+  tags?:string[]
+}
